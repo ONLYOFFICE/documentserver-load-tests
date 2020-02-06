85a18081c78ac9aeba1c5a0d6b73793bc20a39a8
Dmitriy Rotatyy <flamine@list.ru>
2020-02-06 17:40:42 +0300

    add ability to use websocket in plugin
    It need for check activity of editors

--- src/public/plugin/app.js
+++ src/public/plugin/app.js
@@ -1,36 +1,41 @@
 window.Asc.plugin.init = async function () {
-  let j = 0;
-  Asc.scope.r = Math.floor(Math.random() * 254);
-  Asc.scope.g = Math.floor(Math.random() * 254);
-  Asc.scope.b = Math.floor(Math.random() * 254);
-  console.log(Asc.scope.r);
-  console.log(Asc.scope.g);
-  console.log(Asc.scope.b);
+    let j = 0;
+    Asc.scope.r = Math.floor(Math.random() * 254);
+    Asc.scope.g = Math.floor(Math.random() * 254);
+    Asc.scope.b = Math.floor(Math.random() * 254);
+    console.log(Asc.scope.r);
+    console.log(Asc.scope.g);
+    console.log(Asc.scope.b);
 
-for (let i = 0; i < 100; i) {
-    await write();
-}
-  
-  function write() {
-    j += 1;
-    return new Promise(resolve => {
-      window.Asc.plugin.callCommand(function () {
-        var oDocument = Api.GetDocument();
-        var oParagraph, oParaPr;
-        var oParaStyle = oDocument.GetDefaultStyle("paragraph");
-        oParagraph = oDocument.GetElement(Math.floor(Math.random() * 1320));
-        oParagraph.SetStyle(oParaStyle);
-        oParaPr = oParagraph.GetParaPr();
-        oParaPr.SetBetweenBorder("single", 24, 0, Asc.scope.r, Asc.scope.g, Asc.scope.b);
-      }, false, '',  function () {
-        console.log(window.Asc.plugin.info)
-          let xhr = new XMLHttpRequest();
-          xhr.open("POST", window.location.origin + '/activity', true);
-          xhr.setRequestHeader('Content-Type', 'application/json');
-          xhr.send(JSON.stringify({ username: window.Asc.plugin.info.userName,
-            key: window.Asc.plugin.info.documentId}));
-        setTimeout(() => resolve(), 1000);
-      });
-    })
-  }
+    let socket = new WebSocket('ws://' + window.location.host + '/activity');
+    const sessionData = JSON.stringify({
+        username: window.Asc.plugin.info.userName,
+        key: window.Asc.plugin.info.documentId
+    });
+
+    for (let i = 0; i < 100; i) {
+        await write();
+        if (socket.readyState === 1) {
+            socket.send(sessionData);
+        } else if (socket.readyState === 3) {
+            socket = new WebSocket('ws://' + window.location.host + '/activity');
+        }
+    }
+
+    function write() {
+        j += 1;
+        return new Promise(resolve => {
+            window.Asc.plugin.callCommand(function () {
+                var oDocument = Api.GetDocument();
+                var oParagraph, oParaPr;
+                var oParaStyle = oDocument.GetDefaultStyle("paragraph");
+                oParagraph = oDocument.GetElement(Math.floor(Math.random() * 1320));
+                oParagraph.SetStyle(oParaStyle);
+                oParaPr = oParagraph.GetParaPr();
+                oParaPr.SetBetweenBorder("single", 24, 0, Asc.scope.r, Asc.scope.g, Asc.scope.b);
+            }, false, '', function () {
+                setTimeout(() => resolve(), 1000);
+            });
+        })
+    }
 };
