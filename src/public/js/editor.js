471167365b2d493467968e17553d52b8753dbc5f
Dmitriy Rotatyy <flamine@list.ru>
2020-02-13 12:05:17 +0300

    delete open plugin automaticly

--- src/public/js/editor.js
+++ src/public/js/editor.js
@@ -30,3 +30,33 @@ function open_and_wait(key, userName, exampleUrl) {
     }
     window.instance = new DocsAPI.DocEditor("editor_frame", config);
 }
+
+function open_without_plugin(key, userName, exampleUrl) {
+    const config = {
+        documentType: 'text',
+        document: {
+            fileType: "docx",
+            url: exampleUrl + "/public/documents/Document1.docx",
+        },
+        editorConfig: {
+            mode: 'edit',
+            callbackUrl: exampleUrl + '/callback',
+            user: {
+                name: userName
+            },
+            plugins: {
+                pluginsData: [
+                    exampleUrl + "/public/plugin/config.json"
+                ]
+            },
+        },
+    };
+    if (key) {
+        config['document']['key'] = key
+    }
+
+    if (this.userName) {
+        config['editorConfig']['user'] = {"name": this.userName}
+    }
+    window.instance = new DocsAPI.DocEditor("editor_frame", config);
+}
