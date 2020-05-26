d66c128fa1460095262da2dd81d516e1f47dbb02
Dmitriy Rotat <kvazilife@gmail.com>
2020-05-26 17:52:33 +0300

    code style for plugin

--- src/public/plugins/background_for_cells/app.js
+++ src/public/plugins/background_for_cells/app.js
@@ -1,37 +1,41 @@
-window.Asc.plugin.init = async function () {
-    let j = 0;
-    Asc.scope.r = Math.floor(Math.random() * 254);
-    Asc.scope.g = Math.floor(Math.random() * 254);
-    Asc.scope.b = Math.floor(Math.random() * 254);
-    console.log(Asc.scope.r);
-    console.log(Asc.scope.g);
-    console.log(Asc.scope.b);
-
-    let socket = new WebSocket('ws://' + window.location.host + '/message');
-    const sessionData = JSON.stringify({
-        username: window.Asc.plugin.info.userName,
-        key: window.Asc.plugin.info.documentId
-    });
-
-    setInterval(async function () {
-        console.time('write');
-        await write();
-        console.timeEnd('write');
-        if (socket.readyState === 1) {
-            socket.send(sessionData);
-        } else if (socket.readyState === 3) {
-            socket = new WebSocket('wss://' + window.location.host + '/message');
-        }
-    }, 500);
-
-    function write() {
-        j += 1;
-        return new Promise(resolve => {
-            window.Asc.plugin.callCommand(function () {
-                const oWorksheet = Api.GetActiveSheet();
-                const range = oWorksheet.GetRangeByNumber(Math.floor(Math.random() * 28), Math.floor(Math.random() * 40));
-                range.SetFillColor(Api.CreateColorFromRGB(Asc.scope.r, Asc.scope.g, Asc.scope.b));
-            }, false, '', resolve);
-        })
+window.Asc.plugin.init = async function() {
+  // eslint-disable-next-line no-unused-vars
+  let j = 0;
+  Asc.scope.r = Math.floor(Math.random() * 254);
+  Asc.scope.g = Math.floor(Math.random() * 254);
+  Asc.scope.b = Math.floor(Math.random() * 254);
+  console.log(Asc.scope.r);
+  console.log(Asc.scope.g);
+  console.log(Asc.scope.b);
+  let socket = new WebSocket('ws://' + window.location.host + '/message');
+  const sessionData = JSON.stringify({
+    username: window.Asc.plugin.info.userName,
+    key: window.Asc.plugin.info.documentId,
+  });
+  setInterval(async function() {
+    console.time('write');
+    await write();
+    console.timeEnd('write');
+    if (socket.readyState === 1) {
+      socket.send(sessionData);
+    } else if (socket.readyState === 3) {
+      socket = new WebSocket('ws://' + window.location.host + '/message');
     }
+  }, 500);
+
+  /**
+  * metot need for loging every change in special session.
+  * @return {Promise}
+   */
+  function write() {
+    j += 1;
+    return new Promise((resolve) => {
+      window.Asc.plugin.callCommand(function() {
+        const oWorksheet = Api.GetActiveSheet();
+        const range = oWorksheet.GetRangeByNumber(Math.floor(Math.random() * 28),
+            Math.floor(Math.random() * 40));
+        range.SetFillColor(Api.CreateColorFromRGB(Asc.scope.r, Asc.scope.g, Asc.scope.b));
+      }, false, '', resolve);
+    });
+  }
 };
